5e4c0d10d5409137a7fffd55faaf2d616fdc6d4c
yumna <yumnashabbir2@gmail.com>
2025-08-19 10:46:29 +0500

    Employee Class

--- src/main/java/com/example/apidatafetcher/ApiDataFetcherApplication.java
+++ src/main/java/com/example/apidatafetcher/ApiDataFetcherApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiDataFetcherApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiDataFetcherApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDataFetcherApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/apidatafetcher/ApiDataFetcherApplicationTests.java
+++ src/test/java/com/example/apidatafetcher/ApiDataFetcherApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApiDataFetcherApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
