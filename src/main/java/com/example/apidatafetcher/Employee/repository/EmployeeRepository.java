6251f72b1bb8d7799b04880bc4e417319cffc194
yumna <yumnashabbir2@gmail.com>
2025-08-22 11:26:07 +0500

    Employee Respository

--- src/main/java/com/example/apidatafetcher/Employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/apidatafetcher/Employee/repository/EmployeeRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
@@ -16,5 +18,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     @Transactional
     @Query(value = "UPDATE employees SET salary = salary + 5000 WHERE department = :dept", nativeQuery = true)
     int increaseSalaryByDepartment(@Param("dept") String department);
+
+    Optional<Employee> findByEmail(String email);
 }
 
