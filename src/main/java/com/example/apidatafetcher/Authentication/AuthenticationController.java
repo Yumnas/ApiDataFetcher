fe2938e09df79bd72fa44399b12ce0e48ab107ea
yumna <yumnashabbir2@gmail.com>
2025-08-22 11:23:46 +0500

    Auth controller

--- src/main/java/com/example/apidatafetcher/Authentication/AuthenticationController.java
+++ src/main/java/com/example/apidatafetcher/Authentication/AuthenticationController.java
@@ -1,4 +1,46 @@
 package com.example.apidatafetcher.Authentication;
 
+import com.example.apidatafetcher.Employee.entity.Employee;
+import com.example.apidatafetcher.Employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
 public class AuthenticationController {
+    private final JwtService jwtUtil = new JwtService();
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login(@RequestBody Employee request) {
+
+        Optional<Employee> employeeOpt = employeeRepository.findByEmail(request.getEmail());
+
+        if (employeeOpt.isPresent()) {
+            Employee employee = employeeOpt.get();
+
+            // 2. check password
+            if (passwordEncoder.matches(request.getPassword(), employee.getPassword())) {
+                String token = jwtUtil.generateToken(employee);
+                Map<String, String> response = new HashMap<>();
+                response.put("token", token);
+                return ResponseEntity.ok(response);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+    }
 }
